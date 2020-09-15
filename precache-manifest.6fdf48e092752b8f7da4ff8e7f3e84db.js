27e9aa0a2a92d1b2a20c0b24d94641222d5ef446
roshanislimitless <roshan.sapkota2016@gmail.com>
2020-09-15 17:02:39 +0200

    Updates

---
+++ precache-manifest.6fdf48e092752b8f7da4ff8e7f3e84db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5314d8a320941e985df5cbdeb6243de",
+    "url": "/amazona/index.html"
+  },
+  {
+    "revision": "b1b587b58f948e5eea10",
+    "url": "/amazona/static/css/main.cdaa560d.chunk.css"
+  },
+  {
+    "revision": "2307611057975804f4e1",
+    "url": "/amazona/static/js/2.e5faeb72.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/amazona/static/js/2.e5faeb72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1b587b58f948e5eea10",
+    "url": "/amazona/static/js/main.2cdd5015.chunk.js"
+  },
+  {
+    "revision": "1c551499ba3b6cf040a7",
+    "url": "/amazona/static/js/runtime-main.f64973b0.js"
+  }
+]);
